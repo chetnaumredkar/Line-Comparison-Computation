7a21897967b323a5633dd9a19a8479755bf780c2
kolhechetna <kolhe.chetna@gmail.com>
2022-10-30 18:58:02 +0530

    Added:Starting Of line Program

---
+++ src/com/bridgelabz/Line.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Line {
+public static void main(String[] args) {
+	System.out.println("Welcome to Line\r\n"
+			+ "Comparison Computation\r\n"
+			+ "Program on Master Branch ");
+}
+}
